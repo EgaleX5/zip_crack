abaada418bf79d964ae2496e2dd249d80270b92d
EgaleX5 <gameraditya3703@gmail.com>
2025-01-26 22:14:54 +0530

    Added missing files for zip cracking tool

--- decrypt_script.py
+++ decrypt_script.py
@@ -6,8 +6,8 @@ with open('encryption.key', 'rb') as key_file:
 
 cipher_suite = Fernet(key)
 
-# Decrypt the message
-encrypted_message = b'Encrypted message here'  # Replace with actual encrypted message
+# Encrypted message from the output of encrypt_script.py
+encrypted_message = b'gAAAAABnf35ffyjvmWDJHfhUaqChufcHyGAB0RlLMoiwIe3uEszIDHmXfpXdKWSPWDNlcyUkqQycbl29UGBPHdy2qgMuj1_LG0tRzrN83N-9tyVRwLJ6hWs='  # Your encrypted message here
 decrypted_message = cipher_suite.decrypt(encrypted_message)
 
 print(f"Decrypted Message: {decrypted_message.decode()}")
